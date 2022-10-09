54b1051503a57931953ec939d7feb5afd2e8a3e4
MikkoMallikas85 <n0romi00@students.oamk.fi>
2022-10-09 15:32:12 +0300

    first commit

---
+++ Styles.js
@@ -0,0 +1,33 @@
+import { StyleSheet } from "react-native"
+
+export default styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingTop: 28
+      },
+      field: {
+        margin: 15
+      },
+      input: {
+        marginLeft: 15
+      },
+      radio: {
+        marginTop: 15,
+        marginBottom: 15
+      },
+      picker: {
+        margin: 15
+      },
+      heading: {
+        color: 'smoke',
+        fontSize: 25,
+        fontWeight: 'bold',
+      },
+      output: {
+        fontSize: 38,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        backgroundColor: 'hsl(217, 0%, 20%)',
+        borderRadius: 4
+      }
+})
